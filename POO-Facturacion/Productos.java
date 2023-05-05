4b1ee448f6373d8992574ba4804fc0b98dc99709
BraulioOsorio <braulioosoriomartinez@gmail.com>
2023-05-05 07:16:32 -0500

    Avances de la Factura

--- POO-Facturacion/Productos.java
+++ POO-Facturacion/Productos.java
@@ -3,14 +3,17 @@ public class Productos{
     double[] precios;
     String[] nombres;
 
-}
-public Productos(){
-    this.precios = new double[10]{ 24.580, 4.230, 1.500, 13.456, 8.000, 2.000, 4.653, 10.020, 5.300, 2.350 };
-    this.nombres = new String[10]{"Aceite","1 libra de Arroz","Media Libra de queso","Leche en polvo","Zanahoria","Carbe","Bocadillo","Libra de carne","Pira","Panela"};
-       
+
+
+    public Productos(){
+        this.precios = new double[10]{ 24.580, 4.230, 1.500, 13.456, 8.000, 2.000, 4.653, 10.020, 5.300, 2.350 };
+        this.nombres = new String[10]{"Aceite","1 libra de Arroz","Media Libra de queso","Leche en polvo","Zanahoria","Carbe","Bocadillo","Libra de carne","Pira","Panela"};  
+    }
+
+    public void facturar(){
+        System.out.print("aja");
+    }
 }
 
 
-public void facturar(){
-    System.out.print("aja");
-}
+
