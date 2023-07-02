ab3226b4f304823ae631e2a2d27bf082dd651e3a
BraulioOsorio <Braulioosoriomartinez@gmail.com>
2023-07-02 11:02:00 -0500

    Listo

--- CICLOS/Ejercicio20.java
+++ CICLOS/Ejercicio20.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
-public class Ejercicio20{
-    public static void main (String[]args){
-        Scanner entrada=new Scanner(System.in);
-        System.out.print("ingrese un numero-> ");
-        int numero = entrada.nextInt();
-        int k=0;
-          for(int i=1; i<=numero;i++){
-            for(k=1;k<=(numero);k++){
-                int entero=0;   
-                entero+=k;
-                System.out.print(entero)  ; 
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese número N: ");
+        int N = scanner.nextInt();
+        
+        int[][] cuadradoLatino = new int[N][N];
+        
+        for (int i = 0; i < N; i++) {
+            cuadradoLatino[0][i] = i + 1;
+        }
+        
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cuadradoLatino[i][j] = cuadradoLatino[i - 1][(j + 1) % N];
             }
-            System.out.println("");
+        }
+        
+        System.out.println("Cuadrado latino:");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(cuadradoLatino[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
